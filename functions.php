f809cc52dc29ffb2a155c7c72ec18b2d70a8d6ec
Paulo Freitas <paulonprinfor@yahoo.com.br>
2022-06-16 20:17:12 -0300

    Adicionar o suporte do woocommerce

--- functions.php
+++ functions.php
@@ -22,6 +22,27 @@ if ( function_exists( 'register_nav_menu' ) ) {
   register_nav_menu( 'footer-menu', 'Menu do Rodapé');
 }
 
+add_theme_support('woocommerce', array(
+  'thumbnail_image_width'   => 255,
+    'single_image_width'    =>  255,
+    'product_grid'          => array(
+          'default_rows'    => 10,
+          'min_rows'        => 5,
+          'max_rows'        => 10,
+          'default_columns' => 3,
+          'min_columns'     => 3,
+          'max_columns'     => 3,
+  )
+));
+
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
+
+if ( ! isset( $content_width ) ) {
+  $content_width = 600;
+}
+
 function get_img_uri() {
   return get_template_directory_uri() . '/src/img';
 }
